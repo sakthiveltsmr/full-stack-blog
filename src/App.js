42c05d99ec1ccbe32adacf152496090866dde5ad
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-01-31 20:24:51 +0530

    top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
+import Topbar from "./Components/Topbar/Topbar";
 
 function App() {
   // const currentUser = true;
   return (
     <Router>
+      <Topbar />
       <Routes>
         <Route exact path="/" element={<Homepage />} />
       </Routes>
