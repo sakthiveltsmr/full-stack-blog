f553524c6220c6aff963ca7a1090e95ab6e21126
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-08 14:01:12 +0530

    Register

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from "./Pages/Homepage/Homepage";
 import Topbar from "./Components/Topbar/Topbar";
 import About from "./Components/About/About";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   // const currentUser = true;
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
+export default function Register() {
+  //states
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [err, setError] = useState(false);
+  //handleSubmit
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setError(false);
+      window.location.replace("/login");
+    } catch (error) {
+      setError(true);
+      console.log("Error is:", error);
+    }
+  };
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerform" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter Your Username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          className="registerInput"
+          placeholder="Enter Your Email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter Your Password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">
+          Register
+        </button>
+        {err && (
+          <span
+            style={{
+              textAlign: "center",
+              color: "#afd",
+              fontWeight: "bold",
+              fontSize: "20px",
+              padding: "7px",
+            }}
+          >
+            Something Went Wrong
+          </span>
+        )}
+      </form>
+      <button className="LoginButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
+    </div>
+  );
+}
